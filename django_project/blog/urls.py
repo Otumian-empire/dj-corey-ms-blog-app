2983e93ba111873669a3f36f2ce8b9fa43c7f541
otumian-empire <popecan1000@gmail.com>
2020-10-03 15:08:32 +0000

    url view settings static template

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 # from .models import *
 from . import views
 
+appname = 'blog'
+
 urlpatterns = [
     path('', views.home, name="blog-home"),
     path('about/', views.about, name="blog-about"),
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': "Daniel",
+        'title': "Who Goes There",
+        'date_posted': "2nd December, 2000",
+        'content': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi ullam ad quaerat rem libero sequi, tenetur velit cumque explicabo magni dolore eum dolores corrupti cupiditate unde omnis illum aliquid possimus?"
+    },
+    {
+        'author': "Michael",
+        'title': "What Went There",
+        'date_posted': "22nd Match, 2002",
+        'content': "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi ullam ad quaerat rem libero sequi, tenetur velit cumque explicabo magni dolore eum dolores corrupti cupiditate unde omnis illum aliquid possimus?"
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Hello there!</h1>')
+    context = { "posts": posts }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Hello there about!</h1>')
+    return render(request, 'blog/about.html', {'title': 'About Us'})
+
